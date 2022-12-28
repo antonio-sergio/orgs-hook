2f344ab4332e3f8c74995b5de835ad2a227355f9
Antonio Alves <antonioalves.dev@outlook.com>
2022-12-28 18:06:24 -0300

    add

---
+++ src/telas/Home/componentes/Produtores.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text } from "react-native";
+import { carregaProdutores } from "../../../servicos/carregaDados";
+
+export default function Produtores({topo: Topo}){
+    const [titulo, setTitulo] = useState('');
+    const [lista, setLista] = useState([]);
+    useEffect(() => {
+        const retorno = carregaProdutores();
+        setTitulo(retorno.titulo);
+        setLista(retorno.lista);
+        console.log(retorno);
+    }, [])
+
+    const TopoLista = () => {
+        return <>
+            <Topo />
+            <Text style={estilos.titulo}>{titulo}</Text>
+        </> 
+    }
+    return <FlatList
+        data={lista}
+        renderItem={({item: {nome}}) => <Text>{nome}</Text>}
+        ListHeaderComponent={TopoLista}/>
+}
+
+
+const estilos = StyleSheet.create({
+    titulo: {
+        fontSize: 20,
+        lineHeight: 32,
+        marginHorizontal: 16,
+        marginTop: 16,
+        fontWeight: 'bold',
+        color: '#464646'
+    }
+})
---
+++ src/telas/Home/index.js
@@ -0,0 +1,13 @@
+import Produtores from './componentes/Produtores';
+import Topo from './componentes/Topo';
+
+function Home() {
+  return (
+    <>
+    <Produtores topo={Topo}/>
+    </>
+  );
+}
+
+
+export default Home;
